2d00945df7c19eddc36dea2f800fc85552e40a0c
Kim Laage <k.laage@gmail.com>
2019-11-05 22:32:30 +0100

    Test add badge to README

--- 002-Show-AsteriskDiamond.ps1
+++ 002-Show-AsteriskDiamond.ps1
@@ -1,4 +1,17 @@
 <#
+Your task is to return a string that displays a diamond shape on the screen using 
+asterisk (“*”) characters.
+
+The shape that the print method will return should resemble a diamond. A number 
+provided as input will represent the number of asterisks printed on the middle 
+line. The line above and below will be centered and will have two less asterisks 
+than the middle line. This reduction will continue for each line until a line with 
+a single asterisk is printed at the top and bottom of the figure.
+
+Return null if input is an even number or a negative number.
+#>
+
+<#
 .SYNOPSIS
   Short description
 .DESCRIPTION
@@ -19,23 +32,18 @@ function Show-AsteriskDiamond {
    "This is either a negative number or an even number. Try again!" 
   }
   else {
+    # NOTE try creating a string variable and use .substring() -replace 
+    $CountDownStars = $Stars
+    $CountUpStars = 1
     "*" * $Stars
+    do {
+      $CountDownStars = $CountDownStars - 2
+      "*" * $CountDownStars
+    } until ($CountDownStars -eq 1);
   }
 }
 
-<#
-Your task is to return a string that displays a diamond shape on the screen using 
-asterisk (“*”) characters.
-
-The shape that the print method will return should resemble a diamond. A number 
-provided as input will represent the number of asterisks printed on the middle 
-line. The line above and below will be centered and will have two less asterisks 
-than the middle line. This reduction will continue for each line until a line with 
-a single asterisk is printed at the top and bottom of the figure.
-
-Return null if input is an even number or a negative number.
-#>
-
+# Calling the function!
 Show-AsteriskDiamond
 Show-AsteriskDiamond -Stars 2
 Show-AsteriskDiamond -Stars -3
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DevChallenge
+
 Dev.to daily challenge repo. Powershell-centric
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
